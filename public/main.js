1b7ebada33570557617bb43401792df84083085a
andrewlin573 <lin.andr@husky.neu.edu>
2021-01-04 21:34:40 -0500

    Rete

---
+++ public/main.js
@@ -0,0 +1,38 @@
+const update = document.querySelector('#increase-weight');
+document.getElementById("home").style.backgroundColor = "#92C4EE";
+
+update.addEventListener('click', _ => {
+    fetch('/exercises', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name: 'Barbell Press',
+            weight: '90'
+        })
+    }).then(res => {
+        if (res.ok) return res.json();
+    }).then(response => {
+        window.location.reload();
+    }).catch(error => console.error(error))
+})
+
+const deleteButton = document.querySelector("#delete-exercise");
+const messageDiv = document.querySelector('#message');
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/exercises', {
+        method: 'delete',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            name: 'Barbell Press'
+        })
+    }).then(res => {
+        if (res.ok) return res.json();
+    }).then(response => {
+        if (response === "No Barbell Exercise to delete") {
+            messageDiv.textContent = 'No Barbell Exercise to delete'
+        } else {
+            window.location.reload();
+        }
+    }).catch(error => console.error(error))
+})
---
+++ server.js
@@ -0,0 +1,92 @@
+console.log("May Node be with you");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+const connectionString = 'mongodb+srv://andrewlin573:Alin4523$$@cluster0.bv2vu.mongodb.net/andrewlin573?retryWrites=true&w=majority'
+
+const port = 3000;
+
+MongoClient.connect(connectionString, {useUnifiedTopology: true})
+    .then(client => {
+    console.log('Connected to Database');
+    const db = client.db('WorkoutTrackerDB');
+    const exercisesCollection = db.collection('exercises');
+
+    app.set('view engine', 'ejs');
+    app.use(bodyParser.urlencoded({extended: true}));
+    app.use(bodyParser.json());
+    app.use(express.static('public'));
+
+    app.get("/", (req, res) => {
+        db.collection("exercises").find().toArray().then(results => {
+            res.render('index.ejs', {exercises: results});
+        }).catch(error => console.error(error))
+    })
+
+    app.get('/home', (req, res) => {
+        db.collection("exercises").find().toArray().then(results => {
+            res.render('index.ejs', {exercises: results});
+        }).catch(error => console.error(error))
+    })
+
+    app.get('/workouts', (req, res) => {
+        db.collection("exercises").find().toArray().then(results => {
+            res.render('workouts.ejs', {exercises: results});
+        }).catch(error => console.error(error))
+    })
+
+    app.get('/analytics', (req, res) => {
+        db.collection("exercises").find().toArray().then(results => {
+            res.render('analytics.ejs', {exercises: results});
+        }).catch(error => console.error(error))
+    })
+    
+    app.get('/login', (req, res) => {
+        db.collection("exercises").find().toArray().then(results => {
+            res.render('login.ejs', {exercises: results});
+        }).catch(error => console.error(error))
+    })
+
+    app.post('/exercises', (req, res) => {
+        exercisesCollection.insertOne(req.body).then(result => {
+            res.redirect('/');
+        }).catch(error => console.error(error))
+    })
+
+    app.put('/exercises', (req, res) => {
+        exercisesCollection.findOneAndUpdate(
+            {name: 'Dumbell Press'},
+            {
+                $set: {
+                    name: req.body.name,
+                    weight: req.body.weight
+                }
+            },
+            {
+                upsert: true
+            }
+        ).then(result => {
+            res.json('Success');
+        }).catch(error => console.error(error))
+    })
+
+    app.delete('/exercises', (req, res) => {
+        exercisesCollection.deleteOne(
+            {name: req.body.name},
+        ).then(result => {
+            if (result.deletedCount === 0) {
+                return res.json('No Barbell Exercise to delete');
+            }
+            res.json('Deleted a Barbell Exercise');
+        }).catch(error => console.error(error))
+    })
+
+
+    app.listen(port, function() {
+        console.log('listening on 3000');
+    })
+
+}).catch(error => console.error(error))
+
